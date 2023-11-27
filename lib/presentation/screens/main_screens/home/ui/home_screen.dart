07a338623bd86892477058382199839dd831a630
MayadaMansour <mayadamansour1994@gmail.com>
2023-11-27 16:22:02 +0200

    Slider Edit

--- lib/presentation/screens/main_screens/home/ui/home_screen.dart
+++ lib/presentation/screens/main_screens/home/ui/home_screen.dart
@@ -13,37 +13,37 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorResources.background,
-      appBar: AppBar(
-        backgroundColor: ColorResources.background,
-        title: Text(
-          "الصفحه الرئيسيه",
-          style: TextStyle(
-              color: ColorResources.mainColor,
-              fontSize: 20,
-              fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-        elevation: 0,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 10),
-            child: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.notification_add,
-                  color: ColorResources.mainColor,
-                  size: 30,
-                )),
-          )
-        ],
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: ColorResources.background,
+      //   title: Text(
+      //     "الصفحه الرئيسيه",
+      //     style: TextStyle(
+      //         color: ColorResources.mainColor,
+      //         fontSize: 20,
+      //         fontWeight: FontWeight.bold),
+      //   ),
+      //   centerTitle: true,
+      //   automaticallyImplyLeading: false,
+      //   elevation: 0,
+      //   actions: [
+      //     Padding(
+      //       padding: const EdgeInsets.only(right: 10),
+      //       child: IconButton(
+      //           onPressed: () {},
+      //           icon: Icon(
+      //             Icons.notification_add,
+      //             color: ColorResources.mainColor,
+      //             size: 30,
+      //           )),
+      //     )
+      //   ],
+      // ),
       body: SafeArea(
           child: SizedBox(
         height: MediaQuery.of(context).size.height,
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.all(10),
+            padding: const EdgeInsets.only(top: 20,right: 10,left: 10),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.end,
